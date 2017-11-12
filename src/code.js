18c9b0e3a038c15d02b9116fd9d6059b3451d58f
Alicja <alicja.x.lukaszewska@gmail.com>
2017-11-12 22:51:09 +0100

    added transitions and dropdown menu

--- src/code.js
+++ src/code.js
@@ -4,3 +4,19 @@ for (var i = 1; i < document.links.length; i++) {
         document.links[i].className = 'active';
     }
 }
+
+var navbarToggleBtn = document.querySelector('.navbar-toggle');
+var dropdown = document.querySelector('.dropdown');
+
+function showNavbar() {
+	let display = window.getComputedStyle(dropdown,null).getPropertyValue("display");
+	if (display === 'none'){
+		dropdown.style.display = 'block';
+		dropdown.classList.add('activateDropdown');
+	} else {
+		dropdown.style.display = 'none';
+		dropdown.classList.remove('activateDropdown');
+	}
+}
+
+navbarToggleBtn.addEventListener('click', showNavbar);
